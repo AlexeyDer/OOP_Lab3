d2f922b4ea126d5de2d2bcb8f7805b68b9e4c6c8
LittleHell13 <hottelmich@gmail.com>
2019-09-29 16:25:52 +0700

    Add MoveHaot for OX

--- src/tPoint.java
+++ src/tPoint.java
@@ -6,9 +6,14 @@ public class tPoint {
 	public boolean bx = true, by = true;
 	Random random = new Random();
 
+	private int count_x;
+	private int count_y;
+
 	private tPoint(int window_w, int window_h) {
 		this.x = random.nextInt(window_w);
 		this.y = random.nextInt(window_h);
+		this.count_x = random.nextInt(2);
+		this.count_y = random.nextInt(2);
 	}
 
 	public void MovePramX() {
@@ -22,12 +27,46 @@ public class tPoint {
 				bx = true;
 		}
 	}
-	
-	public void MoveHaot() {
-		
+
+	private int moveCords(int a) {
+		if (a == 0)
+			return 1;
+		else
+			return 0;
 	}
 
-	
+	public void MoveHaot() {
+
+		if (count_x == 0 && bx && x < window_w - 15)
+			++this.x;
+		else {
+			if (bx) {
+				count_x = moveCords(count_x);
+			}
+			bx = false;
+			count_x = moveCords(count_x);
+
+			if (count_x == 1 && !bx && x > 1)
+				--this.x;
+			else {
+				if (!bx) {
+					count_x = moveCords(count_x);
+				}
+				bx = true;
+			}
+		}
+
+		if (count_x == 0)
+			++this.x;
+		else
+			--this.x;
+
+//		if (count_y == 0 && y < window_h)
+//			++this.y;
+//		else
+//			--this.y;
+
+	}
 
 	public static void main(String args[]) {
 		final int window_w = 1300, window_h = 750;
