e876bc2362e4c4274b4c66c0948435cb88744dcd
LittleHell13 <hottelmich@gmail.com>
2019-09-29 13:14:53 +0700

    Formating code

--- src/Window.java
+++ src/Window.java
@@ -5,13 +5,13 @@ public class Window extends JFrame {
 	private tPoint[] point;
 	private int n;
 	public static final int window_w = 1300, window_h = 750;
-	
+
 	JFrame frame;
-	
-	public Window (int window_w, int window_h, tPoint[] point, int n) {
+
+	public Window(int window_w, int window_h, tPoint[] point, int n) {
 		this.point = point;
 		this.n = n;
-		
+
 		frame = new JFrame("tPoint");
 		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		frame.setSize(window_w, window_h);
@@ -20,17 +20,17 @@ public class Window extends JFrame {
 		frame.setLocationRelativeTo(null);
 		MoveIt();
 	}
-	
-	
+
 	private void MoveIt() {
-		while(true) {
+		while (true) {
 			for (int i = 0; i < n; i++)
 				point[i].MovePramX();
-			try{
-	                Thread.sleep(10);
-	            } catch (Exception exc){}
-	            frame.repaint();
-			
+			try {
+				Thread.sleep(10);
+			} catch (Exception exc) {
+			}
+			frame.repaint();
+
 		}
 	}
 }
